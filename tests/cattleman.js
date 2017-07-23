5e06435e6050396350227efe087d54c49def50e4
Chlorophyllkid <s3chvoig@gmail.com>
2017-07-23 10:34:34 +0200

    added more tests

--- tests/cattleman.js
+++ tests/cattleman.js
@@ -1,8 +1,32 @@
+'use strict'
+
 const path = require('path')
+const fs = require('fs')
+
 const test = require('tape')
 const Cattleman = require('../lib/cattleman')
 
 test('Cattleman test cases:', t => {
+
+    t.test('Setup a source directory', assert => {
+        fs.mkdirSync('src')
+        fs.writeFileSync('src/base.js', 'console.log("base.js")')
+        fs.writeFileSync('src/base.css', 'body { background: black; }')
+
+        fs.mkdirSync('src/header')
+        fs.writeFileSync('src/header/header.js', 'console.log("header.js")')
+        fs.writeFileSync('src/header/header.test.js', 'console.log("header.test.js")')
+        fs.writeFileSync('src/header/header.css', 'header { background: red; }')
+
+        fs.mkdirSync('src/footer')
+        fs.writeFileSync('src/footer/footer.js', 'console.log("footer.js")')
+        fs.writeFileSync('src/footer/footer.test.js', 'console.log("footer.test.js")')
+        fs.writeFileSync('src/footer/footer.css', 'footer { background: red; }')
+
+        assert.end()
+    })
+
+
 
     t.test('Constructor ...', assert => {
         assert.equal(typeof Cattleman, 'function', '... is function')
@@ -14,41 +38,72 @@ test('Cattleman test cases:', t => {
         assert.end()
     })
 
-
-
     t.test('Method gatherFiles ...', assert => {
-        const cattleman = new Cattleman('lib')
+        const cattleman = new Cattleman('src')
 
         assert.true(cattleman.gatherFiles, '... exists')
         assert.equal(typeof cattleman.gatherFiles, 'function', '... is function')
 
         const list = cattleman.gatherFiles()
+        const comparableList = [
+            'src/base.css',
+            'src/base.js',
+            'src/footer/footer.css',
+            'src/footer/footer.js',
+            'src/header/header.css',
+            'src/header/header.js'
+        ]
         assert.true(list instanceof Array, '... returns array')
         assert.equal(typeof list[0], 'string', '... returns array of strings')
+        assert.deepEqual(list, comparableList, '... works as expected')
 
         const filteredList = cattleman.gatherFiles('.js')
-        assert.equal(path.extname(filteredList[0]), '.js', '... works with extentionFilter')
+        const comparableFilteredList = [
+            'src/base.js',
+            'src/footer/footer.js',
+            'src/header/header.js'
+        ]
+        assert.deepEqual(filteredList, comparableFilteredList, '... works with extentionFilter')
         assert.throws(() => {cattleman.gatherFiles({})}, new Error('extentionFilter has to be type of string'), '... throws error when extentionFilter is no string')
         assert.throws(() => {cattleman.gatherFiles('cattleman')}, new Error('extentionFilter has to start with \'.\''), '... throws error when extentionFilter doesn\'t start with \'.\'')
 
         assert.end()
     })
 
-
-
     t.test('Method gatherEntries ...', assert => {
-        const cattleman = new Cattleman('lib')
+        const cattleman = new Cattleman('src')
 
         assert.true(cattleman.gatherEntries, '... exists')
         assert.equal(typeof cattleman.gatherEntries, 'function', '... is function')
 
         const entries = cattleman.gatherEntries()
-        assert.equal(typeof entries, 'object', '... returns object')
-
-        for(const chunk in entries) {
-            assert.true(entries[chunk] instanceof Array, '... returns object of arrays')
-            assert.equal(typeof entries[chunk][0], 'string', '... returns object of arrays with strings')
+        const comparableEntries = {
+            'base' : [ 'src/base.css', 'src/base.js' ],
+            'footer/footer' : [ 'src/footer/footer.css', 'src/footer/footer.js' ],
+            'header/header' : [ 'src/header/header.css', 'src/header/header.js' ]
         }
+        assert.equal(typeof entries, 'object', '... returns object')
+        assert.deepEqual(entries, comparableEntries, '... works as expected')
+
+        assert.end()
+    })
+
+
+
+    t.test('Teardown the source directory', assert => {
+        fs.unlinkSync('src/header/header.js')
+        fs.unlinkSync('src/header/header.test.js')
+        fs.unlinkSync('src/header/header.css')
+        fs.rmdirSync('src/header', () => {})
+
+        fs.unlinkSync('src/footer/footer.js')
+        fs.unlinkSync('src/footer/footer.test.js')
+        fs.unlinkSync('src/footer/footer.css')
+        fs.rmdirSync('src/footer', () => {})
+
+        fs.unlinkSync('src/base.js')
+        fs.unlinkSync('src/base.css')
+        fs.rmdirSync('src', () => {})
 
         assert.end()
     })
